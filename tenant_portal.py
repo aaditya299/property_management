2f873445929526f1b32cc289fede3973175c1099
Aaditya Jaiswal <obitokakashi299@gmail.com>
2026-07-01 14:30:25 +0530

    Implemented tenant_portal module with active relational loops

--- tenant_portal.py
+++ tenant_portal.py
@@ -19,3 +19,38 @@ def tenant_login():
         print("Error: No resident profile found matching that name.")
         return None
 tenant_profile=tenant_login()
+if tenant_profile:
+    tenant_id, name , pay_status, rent, r_status, address, city=tenant_profile
+    while True:
+        print(f"\nWelcome Home, {name}! (ID: {tenant_id})")
+        print("="*45)
+        print("1. View My Lease & Property Details")
+        print("2. Check My Balance & Rent Status")
+        print("3. Logout & Exit")
+        print("="*45)
+        choice=input("\nSelect an action(1-3): ").strip()
+        
+        if choice=='1':
+            print("\nYour Lease Agreement Details: ")
+            print(f"Property Address: {address},{city}")
+            print(f"Unit Status:      {r_status}")
+            print(f"Commited Rent:    ${rent:,.2f} / month")
+            print("-"*40)
+        elif choice=='2':
+            print("\nCurrent Account Invoice Status:")
+            if pay_status=='paid':
+                print("Status: Paid in Full")
+                print(f"Next Bill Amount: ${rent:,.2f}")
+            elif pay_status=='late':
+                print("Status: Overdue (Late fees may apply!)")
+                print(f"Outstanding Balance Due: ${rent:,.2f}")
+            else:
+                print("Status: Unpaid")
+                print(f"Outstanding Balance Due: ${rent:,.2f}")
+            print("-" * 40)
+        elif choice=='3':
+            print("Logging out of the resident portal")
+            break
+        else :
+            print("\nInvalid chhoice! Please enter a number between 1 and 3.")
+connection.close()
